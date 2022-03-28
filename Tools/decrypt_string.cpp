5120fd4ccbab2b8494f90be10ca07d87775993d9
ALittlePatate <maxime.goyheneche@gmail.com>
2022-03-28 17:20:50 +0200

    Added the string decryption cpp program

---
+++ Tools/decrypt_string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <defs.h>
+
+void Decrypt(int a1, int size)
+{
+    unsigned int i; // [esp+10h] [ebp-4h]
+
+    for (i = 0; i <= (size - 2); ++i)
+        *(char*)(a1 + i) ^= (char)i + 3;
+    *(char*)(a1 + (size - 1)) = 0;
+}
+
+
+int main()
+{
+    char string[] = "Pgdhiagm+jb|/";
+
+    Decrypt((int)string, sizeof(string));
+
+    std::cout << string << std::endl;
+
+    return 0;
+}
